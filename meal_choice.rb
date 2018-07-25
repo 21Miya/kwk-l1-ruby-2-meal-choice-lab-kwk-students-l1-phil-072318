d3d532dd4ddb7ee02237caac8480c3eb308062c1
21Miya <mmshin@live.com>
2018-07-25 13:28:35 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,23 +1,28 @@
-# Here's an example of a 'snacks' method that returns the meal choice passed in to it and defaults to "cheetos" if nothing is passed in.
-def snacks(food="Cheetos")
-  "Any time, is the right time for #{food}!"
-end
 
 # Define breakfast, lunch and dinner methods that return the meal choice passed into them. If nothing is passed in, it shoud default to the foods on the readme (frosted flake, grilled cheese, salmon)
 
+def breakfast(breakfood="frosted flakes")
+  return "Morning is the best time for #{breakfood}!"
+end
 
+def lunch(lunchfood="grilled cheese")
+  return "Noon is the best time for #{lunchfood}!"
+end
 
+def snacks(food="Cheetos")
+  return "Any time, is the right time for #{food}!"
+end 
 
+def dinner(dinnerfood="salmon")
+  return "The evening is the best time for #{dinnerfood}!"
+end
 
+breakfast("food")
+breakfast
+lunch("food")
+snacks
+snacks("yikes")
+lunch
+dinner("food")
+dinner
 
-
-# Call the methods with puts and your own arguments here. Like this:
-puts snacks("Swedish fish")
-## This returns "Any time, is the right time for Sweedish fish!"
-
-# call your methods here
-
-
-# Call your methods without any arguments here
-puts snacks
-## In our snacks method, the default choice is "Cheetos" Calling snacks like this without specifying a choice will return "Any time, is the right time for Cheetos!"
